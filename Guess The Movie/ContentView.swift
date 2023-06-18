46a48f8e87b7021fc7a844fff29239c670c0627e
Mustafaefe <m.efe_1905@hotmail.com>
2023-06-19 00:37:43 +0300

    Application buttons and speaking feature was added.

--- Guess The Movie/ContentView.swift
+++ Guess The Movie/ContentView.swift
@@ -6,16 +6,55 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    
+    let synthesizer = AVSpeechSynthesizer()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Guess the movies")
+                .fontWeight(.bold)
+                .font(.title2)
+            Text("Butonların üzerindeki ikonlardan hangi film olduğunu bulabilir misin?")
+                .padding()
+                .font(.body)
+            
+            Button {
+                speak(text: "Çiçek Taksi")
+            } label: {
+                Text("💐🚕")
+                    .padding()
+            }
+            .background(.yellow)
+            .cornerRadius(10)
+            
+            Button {
+                speak(text: "Babam ve Oğlum")
+            } label: {
+                Text("👨🏻‍🦳 & 👨‍🦱")
+                    .padding()
+            }
+            .background(.green)
+            .cornerRadius(10)
+            
+            Button {
+                speak(text: "Kardeş Payı")
+            } label: {
+                Text("👨‍🦱½👨‍🦱")
+                    .padding()
+            }
+            .background(.purple)
+            .cornerRadius(10)
         }
         .padding()
+    }
+    
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "tr-TR")
+        synthesizer.speak(utterance)
     }
 }
 
