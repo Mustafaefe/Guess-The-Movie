b322e231adf3f79a0d18dbaf1fe05cbc96070e3d
Mustafaefe <m.efe_1905@hotmail.com>
2023-06-19 00:07:55 +0300

    Initial Commit

---
+++ Guess The Movie/Guess_The_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_MovieApp.swift
+//  Guess The Movie
+//
+//  Created by Mustafa Efe on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
